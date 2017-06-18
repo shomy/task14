a4847475c5e9cddbb31d877c768b811225e6715b
shomy <sksks0607@gmail.com>
2017-06-18 14:02:24 +0900

    second commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root'pictures#index'
+  resources :pictures
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
