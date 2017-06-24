de5ae89ee52cfa0a22a72399b2c76b14c0e8d2c0
shomy <sksks0607@gmail.com>
2017-06-24 09:20:11 +0900

    dazn

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -26,7 +26,7 @@ class PicturesController < ApplicationController
   def create
     @picture = Picture.new(picture_params)
     @picture.user_id = current_user.id
-    render :new if @picture.image.invalid?
+    render :new if @picture.invalid?
 
     respond_to do |format|
       if @picture.save
