6c553381d68fc6bd0df59df5e324a058caa8315f
shomy <sksks0607@gmail.com>
2017-06-24 14:17:01 +0900

    l

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -25,15 +25,18 @@ class PicturesController < ApplicationController
   # POST /pictures.json
   def create
     @picture = Picture.new(picture_params)
-    render :new if @picture.invalid?
-    @picture.user_id = current_user.id
-    respond_to do |format|
-      if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        format.json { render :show, status: :created, location: @picture }
-      else
-        format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
+    if @picture.invalid?
+      render 'new'
+    else
+      @picture.user_id = current_user.id
+      respond_to do |format|
+        if @picture.save
+          format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+          format.json { render :show, status: :created, location: @picture }
+        else
+          format.html { render :new }
+          format.json { render json: @picture.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
