37d5c3dd1e97292bfc1525ff05bdb6305d78f25d
shomy <sksks0607@gmail.com>
2017-07-01 13:30:13 +0900

    ko

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
 }
+
+if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+end
+# 省略
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
